9f328ffdcead441d6b0cf5a38e329224f529d332
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2024-04-05 17:02:46 +0530

    Update Second_Draft.jsx

--- React Native/Todo/Second_Draft.jsx
+++ React Native/Todo/Second_Draft.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { Alert, Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { black, red100 } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
+import { black, red100, white } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 import { IconButton } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Tab = createBottomTabNavigator();
 
@@ -69,7 +70,7 @@ const Todo = () => {
 
   const add_to_list = () => {
     if (todo_text !== "") {
-      settodo_list([...todo_list, { id: i, title: todo_text, bg: bg_todo_color}]);
+      settodo_list([...todo_list, { id: i, title: todo_text, bg: bg_todo_color }]);
     }
     settodo_text("");
     i = i + 1;
@@ -113,7 +114,7 @@ const Todo = () => {
   // }
 
   const color_todo_box = () => {
-    return(
+    return (
       gotcolor.map((item, index) => {
         // console.log(item.color.toString());
         return (
@@ -129,7 +130,7 @@ const Todo = () => {
   const rendergotdata = ({ item, index }) => {
     let bgcolor = item.bg
     return (
-      <View style={{...styles.TextBox, backgroundColor:bgcolor}}>
+      <View style={{ ...styles.TextBox, backgroundColor: bgcolor }}>
         <Text style={{ flex: 1, color: "white", fontSize: 15, fontWeight: "bold" }}>{item.title}</Text>
         <Text style={{ flex: 1, color: "white", fontSize: 15, fontWeight: "bold" }}>{item.bg}</Text>
         <IconButton iconColor="white" icon="pencil" onPress={() => edit_todo_id(item)} />
@@ -139,80 +140,93 @@ const Todo = () => {
   };
 
   return (
-    <View style={{ paddingHorizontal: 12 }}>
+      <View style={{ paddingHorizontal: 12, flex: 1, flexDirection: 'column'}}>
+        {
+          todo_list.length === 0 ?
+            <View>
+              <Text style={{ marginTop: 30, fontSize: 35, fontWeight: 500, color: "red" }}>Congrats!</Text>
+              <Text style={{ fontSize: 28, fontWeight: 300 }}>Assigned tasks completed.</Text>
+            </View>
+            :
+            <View>
+              <View style={{ flexDirection: 'row', alignItems: "baseline" }}>
+                <Text style={{ fontSize: 28, fontWeight: 300 }}>Finish </Text>
+                <Text style={{ fontSize: 35, fontWeight: 500, color: "red" }}>Mission!</Text>
+              </View>
+              <FlatList
+                data={todo_list}
+                renderItem={rendergotdata}
+              // horizontal={true}
+              // pagingEnabled={true}
+              />
+            </View>
+        }
 
-      <TextInput
-        style={styles.InputBox}
-        placeholder="Add it !!!"
-        value={todo_text}
-        onChangeText={(text) => {
-          settodo_text(text)
-        }}
-      // onChangeText={(text)=>
-      //   {console.log(text)
-      //   settodo_text(text)}}
-      />
+        <View style={{ paddingVertical: 8, paddingHorizontal: 12, bottom: 0.1, position: 'absolute', justifyContent: "center", backgroundColor: "black", opacity: 0.8 }}>
+          <View style={{ marginTop: 12, flexDirection: "row", gap: 5, justifyContent: "center" }}>{color_todo_box()}</View>
+          <View style={{ flexDirection: "row", gap: 15 }}>
+            <TextInput
+              style={styles.InputBox}
+              placeholder="Add it !!!"
+              placeholderTextColor="white"
+              value={todo_text}
+              onChangeText={(text) => {
+                settodo_text(text)
+              }}
+            // onChangeText={(text)=>
+            //   {console.log(text)
+            //   settodo_text(text)}}
+            />
 
-      <View style={{ flexDirection: "row", gap:5, justifyContent:"center"}}>{color_todo_box()}</View>
-      
-
-      {
-        edit_todo_text ?
-          <TouchableOpacity
-            style={styles.Buttons}
-            // onPress={() => add_to_list()}
-            onPress={() => edit_todo_list()}
-          >
-            <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
-              SAVE
-            </Text>
-          </TouchableOpacity>
-          :
-          <TouchableOpacity
-            style={styles.Buttons}
-            onPress={() => add_to_list()}
-          >
-            <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
-              ADD
-            </Text>
-          </TouchableOpacity>
-      }
-
-      {
-        todo_list.length === 0 ?
-          <View>
-            <Text style={{ marginTop: 30, fontSize: 35, fontWeight: 500, color: "red" }}>Congrats!</Text>
-            <Text style={{ fontSize: 28, fontWeight: 300 }}>Assigned tasks completed.</Text>
+            {
+              edit_todo_text ?
+                <TouchableOpacity
+                  style={styles.Buttons}
+                  // onPress={() => add_to_list()}
+                  onPress={() => edit_todo_list()}
+                >
+                  <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+                    SAVE
+                  </Text>
+                </TouchableOpacity>
+                :
+                <TouchableOpacity
+                  style={styles.Buttons}
+                  onPress={() => add_to_list()}
+                >
+                  <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+                    ADD
+                  </Text>
+                </TouchableOpacity>
+            }
           </View>
-          :
-          <FlatList
-            data={todo_list}
-            renderItem={rendergotdata}
-            horizontal={true}
-          // pagingEnabled={true}
-          />
-      }
-    </View>
+        </View>
+      </View>
   )
 };
 
 const styles = StyleSheet.create({
   InputBox: {
     borderColor: "gray",
+    color: "white",
     borderWidth: 2,
     borderRadius: 6,
     paddingHorizontal: 12,
-    paddingVertical: 16,
-    marginTop: 12,
+    // paddingVertical: 16,
+    height: 60,
+    width: 290,
+    // marginTop: 12,
     marginBottom: 7,
-    fontSize: 20
+    fontSize: 20,
   },
   Buttons: {
-    backgroundColor: "black",
+    // marginTop: 12,
+    backgroundColor: "gray",
     borderWidth: 2,
     borderRadius: 6,
-    paddingHorizontal: 12,
-    paddingVertical: 16,
+    height: 60,
+    width: 70,
+    justifyContent: "center",
     alignItems: "center",
   },
   TextBox: {
